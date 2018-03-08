fa7f04441dcb074b87a859dcd0e37edb5b0f6adf
t1az2z <tyukovia@gmail.com>
2018-03-08 22:20:12 +0300

    Cross platform input begining. Waypoints added. Camera moves accordinly to waypoints.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start () {
+        Invoke("LoadFirstScene", 5f);
+
+	}
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    void Update () {
+		
+	}
+}
